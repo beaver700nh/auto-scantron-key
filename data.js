30b0e94e0a5badd624b5c2d5da72e1a784189276
Minh <beaver700nh@gmail.com>
2023-11-24 18:56:00 -0500

    Added to data, refactored other JS

--- data.js
+++ data.js
@@ -1,34 +1,46 @@
+const Constants = {
+  Margin: 1/32,
+};
+
 const AnswerSheets = {
   "Scantron 19641 Answer Sheet B": {
     dimensions: {
       w: 5 + 1/2,
       h: 11,
     },
+    images: {
+      1: "https://cdn11.bigcommerce.com/s-30cdzu3o6a/images/stencil/1280x1280/products/126/434/19641-6-1__47970.1674581161.jpg?c=1",
+      2: "https://cdn11.bigcommerce.com/s-30cdzu3o6a/images/stencil/1280x1280/products/126/435/19641-6-2__48206.1674581161.jpg?c=1",
+    },
     inputs: {
       meta: {
         name: {
+          page: 1,
           x: 3 + 3/16,
-          y: 9 + 19/32,
+          y: 9 + 9/16,
           w: 2 + 1/16,
           h: 0 + 5/16,
         },
         subject: {
+          page: 1,
           x: 3 + 3/8,
-          y: 9 + 29/32,
+          y: 9 + 7/8,
           w: 1 + 7/8,
           h: 0 + 5/16,
         },
         period: {
+          page: 1,
           x: 3 + 9/32,
-          y: 10 + 7/32,
+          y: 10 + 3/16,
           w: 0 + 13/32,
           h: 0 + 5/16,
         },
         date: {
-          x: 4 + 3/32,
-          y: 10 + 7/32,
-          w: 0 + 13/32,
-          h: 1 + 1/8,
+          page: 1,
+          x: 4 + 1/8,
+          y: 10 + 3/16,
+          w: 1 + 1/8,
+          h: 0 + 5/16,
         },
       },
     },
--- script.js
+++ script.js
@@ -20,17 +20,46 @@ function configureButtonBindings() {
 }
 
 function generate() {
-  const answerSheet = document.querySelector(".ans-sheet").value;
+  const answerSheet = document.querySelector(".ans-sheet");
+  const sheetProperties = AnswerSheets[answerSheet.value];
 
   if (!answerSheet) {
     return;
   }
 
-  const {w, h} = AnswerSheets[answerSheet].dimensions;
+  const {w, h} = sheetProperties.dimensions;
   const pdf = new jspdf.jsPDF({unit: "in", format: [w, h]})
     .setProperties({title: "TODO"})
-    .text("Hello world!", 0, 0.2);
+    .setLineWidth(0.01)
+    .addPage();
 
+  addBackground_DEBUG(pdf, sheetProperties, w, h);
+  addMetadata(pdf, sheetProperties);
+
+  exportPDF(pdf);
+}
+
+function addBackground_DEBUG(pdf, sheetProperties, w, h) {
+  for (const [page, url] of Object.entries(sheetProperties.images)) {
+    pdf.setPage(page);
+    pdf.addImage(url, "JPEG", 0, 0, w, h);
+  }
+}
+
+function addMetadata(pdf, sheetProperties) {
+  for (const [, {page, x, y, w, h}] of Object.entries(sheetProperties.inputs.meta)) {
+    pdf.setPage(page);
+    pdf.rect(x, y, w, -h);
+    pdf.rect(
+      x + Constants.Margin,
+      y - Constants.Margin,
+      w - 2 * Constants.Margin,
+      -h + 2 * Constants.Margin,
+    );
+  }
+}
+
+function exportPDF(pdf) {
   const content = `
     <iframe
       style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; border: none;"
